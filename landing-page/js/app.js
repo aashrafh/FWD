73a661217e41a8ce3f1887eb87788c383c272d66
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-06-18 00:21:32 +0200

    scroll to anchor

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,57 +1,38 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-
+(function() {
+  const unorderedList = document.querySelector("ul");
+  for (let i = 4; i >= 1; i--) {
+    const listElement = `<li class='menu__link box' id="${i}"><a href="#section${i}" >Section ${i}</li>`;
+    unorderedList.insertAdjacentHTML("beforeend", listElement);
+  }
+})();
 
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+(function() {
+  const navList = document.getElementById("navbar__list");
+  navList.addEventListener("click", event => {
+    event.preventDefault();
+    const id = event.target.hasAttribute("id")
+      ? `section${event.target.id}`
+      : `section${event.target.parentElement.id}`;
+    const elem = document.getElementById(id);
+    elem.scrollIntoView({ block: "end", behavior: "smooth" });
+  });
+})();
